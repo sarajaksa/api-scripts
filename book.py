60d3fd749820c3a2fa8e39bc2017e6415d217e64
Sara Jakša <sarajaksa@sarajaksa.eu>
2023-08-05 13:19:40 +0200

    correct cobiss URL

--- book.py
+++ book.py
@@ -11,7 +11,7 @@ def get_cobiss_data(cobiss_url):
     book_page = requests.get("https://" + cobiss_url)
     if not book_page.ok:
         return book_data
-    book_data[BIB_CONSTS.BOOK_LINK] = book_page
+    book_data[BIB_CONSTS.BOOK_LINK] = "https://" + cobiss_url
     if "/mkl-71/" in cobiss_url:
         book_data[BIB_CONSTS.BOOK_LOCATION] = "Mestna Knjižnica Ljubljana - Rudnik"
     if "/mkl/" in cobiss_url:
